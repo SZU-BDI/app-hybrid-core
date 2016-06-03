bbf61647d502d3d111cd1d3e12a6959896b9daa7
twotigershuanghu <shuanghu@zhumindeMacBook-Pro.local>
2016-06-03 18:44:19 +0800

    upload Hybrid v0.3
    upload Hybrid v0.3

---
+++ Hybrid Backup/Hybrid V0.3/Hybrid-v2Tests/Hybrid_v2Tests.m
@@ -0,0 +1,39 @@
+//
+//  Hybrid_v2Tests.m
+//  Hybrid-v2Tests
+//
+//  Created by 双虎 on 16/6/2.
+//  Copyright © 2016年 Cmptech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hybrid_v2Tests : XCTestCase
+
+@end
+
+@implementation Hybrid_v2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Hybrid Backup/Hybrid V0.3/_app_activity_close/ApiActivityClose.h
@@ -0,0 +1,13 @@
+//
+//  ApiActivityClose.h
+//  Hybrid-v2
+//
+//  Created by 双虎 on 16/6/3.
+//  Copyright © 2016年 Cmptech. All rights reserved.
+//
+
+#import "HybridApi.h"
+
+@interface ApiActivityClose : HybridApi
+
+@end
