7efb232d79396663b8dc0347f0827a237162d660
wanjochan <wanjochan@gmail.com>
2016-06-05 20:20:06 +0800

    testproj-ios-core

---
+++ testproj-ios-core/UiRoot/UiRoot.m
@@ -0,0 +1,34 @@
+//
+//  UiRoot.m
+//  Hybrid-v2
+//
+//  Created by 双虎 on 16/6/2.
+//  Copyright © 2016年 Cmptech. All rights reserved.
+//
+
+#import "UiRoot.h"
+
+@interface UiRoot ()
+
+@end
+
+@implementation UiRoot
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self LoadLocalhtmlName:@"root"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:NO];
+    [[self navigationController] setNavigationBarHidden:YES animated:YES];
+}
+
+@end
