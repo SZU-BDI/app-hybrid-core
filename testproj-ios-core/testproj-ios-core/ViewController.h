02318fc37c33a0a7d0a7400b5f931b47afe2ce88
Wanjo Chan <wanjochan@gmail.com>
2016-06-06 10:32:48 +0800

    test for core (iOS) v0.4

---
+++ testproj-ios-core/testproj-ios-core/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testproj-ios-core
+//
+//  Created by wjc on 6/6/16.
+//  Copyright (c) 2016 szu.bdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testproj-ios-core/testproj-ios-core/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testproj-ios-core
+//
+//  Created by wjc on 6/6/16.
+//  Copyright (c) 2016 szu.bdi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
