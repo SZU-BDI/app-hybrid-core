55ff0c6e5d4fd5093719208fa9159561d4bd3bde
twotigershuanghu <dashuju@dashujudeiMac.local>
2016-06-13 17:37:51 +0800

    Backup V0.4
    Backup V0.4

---
+++ backup/Hybrid V0.4/lib-ios/_app_activity_open/ApiActivityOpen.m
@@ -0,0 +1,56 @@
+//
+//  ApiActivityOpen.m
+//  testproj-ios-core
+//
+//  Created by 双虎 on 16/6/2.
+//  Copyright © 2016年 Cmptech. All rights reserved.
+//
+
+#import "ApiActivityOpen.h"
+#import "HybridTools.h"
+#import "HybridUi.h"
+
+@interface ApiActivityOpen ()
+@property (nonatomic, copy) NSDictionary *inputParamData;
+@property (nonatomic, copy) NSString *js_Model;
+@property (nonatomic, copy) NSString *js_address;
+@property (nonatomic, copy) NSString *js_topbar;
+@end
+
+@implementation ApiActivityOpen
+
+- (instancetype)init{
+    if (self = [super init]){
+        _inputParamData = [[NSDictionary alloc] init];
+    }
+    return self;
+}
+
+// 覆盖父类的getHandler方法，并实现
+- (WVJBHandler) getHandler{
+    return ^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"ApiActivityOpen -->  _app_actibity_open");
+        
+        _inputParamData = (NSDictionary *)data;
+        _js_Model = _inputParamData[@"mode"];
+        _js_address = _inputParamData[@"address"];
+        _js_topbar = _inputParamData[@"topbar"];
+        
+        if ([_js_Model isEqualToString:@"WebView"]) {
+            
+            HybridUi *ui = [HybridTools buildHybridUiBase:@"UiContent"];
+            // 传递回调函数
+            ui.jsCallback = responseCallback;
+            // 判断有无yopbar
+            ui.isTopBar = ([_js_topbar isEqualToString:@"Y"])? YES : NO;
+            // 当 _js_Model 为WebView时，有url
+            ui.address = _js_address;
+            NSLog(@"open %@", self.hybridUi);
+            [self.hybridUi.navigationController pushViewController:ui animated:YES];
+            
+        }
+        
+    };
+}
+
+@end
---
+++ backup/Hybrid V0.4/lib-ios/szu-bdi-hybrid-core/HybridUi/HybridUi.h
@@ -0,0 +1,26 @@
+//
+//  HybridUi
+//  testproj-ios-core
+//
+//  Created by 双虎 on 16/6/2.
+//  Copyright © 2016年 Cmptech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WebViewJavascriptBridge.h"
+
+@interface HybridUi : UIViewController
+
+@property (nonatomic, strong) WebViewJavascriptBridge *bridge;
+@property (nonatomic, strong) UINavigationController *topBar;
+@property (nonatomic, strong) UIWebView *webView;
+
+@property (nonatomic, copy) NSString *address;
+@property (nonatomic, strong) WVJBResponseCallback jsCallback;
+@property (nonatomic) BOOL isTopBar;
+
+- (void)LoadLocalhtmlName:(NSString *)loadLocalhtml;
+
+- (void)LoadTheUrl:(NSString *)url;
+
+@end
