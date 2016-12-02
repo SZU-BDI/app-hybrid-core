fa1447f32f8ac6602d59da87705714bf50389e0d
wanjochan <wanjochan@gmail.com>
2016-12-02 17:15:13 +0800

    WJC: ui open cleanup

--- lib-android/szu.bdi.hybrid.core/src/main/java/szu/bdi/hybrid/core/eg/ApiUiOpen.java
+++ lib-android/szu.bdi.hybrid.core/src/main/java/szu/bdi/hybrid/core/eg/ApiUiOpen.java
@@ -2,53 +2,41 @@ package szu.bdi.hybrid.core.eg;
 
 //@doc https://szu-bdi.gitbooks.io/app-hybrid/content/
 
-import android.util.Log;
-
-import szu.bdi.hybrid.core.HybridApi;
-import szu.bdi.hybrid.core.HybridCallback;
-import szu.bdi.hybrid.core.HybridTools;
-import szu.bdi.hybrid.core.HybridUi;
-import szu.bdi.hybrid.core.HybridHandler;
-import szu.bdi.hybrid.core.HybridUiCallback;
+import szu.bdi.hybrid.core.*;
 import info.cmptech.JSO;
 
 
-/**
- *
- *
- */
 public class ApiUiOpen extends HybridApi {
     public HybridHandler getHandler() {
         return new HybridHandler() {
             @Override
             public void handler(String dataStr, final HybridCallback apiCallback) {
                 HybridUi callerUi = getCallerUi();
-                Log.v("_app_activity_open", dataStr);
-
-                String uiName = "UiContent";//default to UiContent
 
                 JSO data_o = JSO.s2o(dataStr);
 
                 String t = data_o.getChild("name").toString();
-                if (!HybridTools.isEmptyString(t)) {
-                    uiName = t;
-                }
+                String uiName = (!HybridTools.isEmptyString(t)) ? t : "UiContent";//default to UiContent
 
                 HybridTools.startUi(uiName, dataStr, callerUi, new HybridUiCallback() {
                     @Override
                     public void onCallBack(final HybridUi ui) {
+
                         //listen "close" event
                         ui.on("close", new HybridCallback() {
                             @Override
                             public void onCallBack(String cbStr) {
+
                                 //fwd
                                 onCallBack(JSO.s2o(cbStr));
                             }
 
                             @Override
                             public void onCallBack(JSO jso) {
+
                                 //manually close it
                                 ui.finish();
+
                                 //api callback
                                 apiCallback.onCallBack(jso);
                             }
@@ -59,6 +47,7 @@ public class ApiUiOpen extends HybridApi {
 
             @Override
             public void handler(JSO jso, HybridCallback cbFunc) {
+                //fwd
                 handler(JSO.o2s(jso), cbFunc);
             }
         };
